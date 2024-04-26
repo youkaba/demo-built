50561704e1d645c186a4d756f208b921e65e465f
ykaba <youkaba2005@gmail.com>
2024-04-26 10:34:09 -0400

    init commit

---
+++ src/test/java/com/example/demobuilt/DemoBuiltApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.demobuilt;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoBuiltApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
